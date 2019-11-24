508e2f845649cd7ae5f538525e83ab79cfc70e97
El Mehdi Kiouane <30392394+kiouanemehdi@users.noreply.github.com>
2019-11-24 22:46:19 +0100

    awdi

--- gestion emploi du temps/admin_acceuil.cs
+++ gestion emploi du temps/admin_acceuil.cs
@@ -119,9 +119,6 @@ namespace gestion_emploi_du_temps
             Environment.Exit(0);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+   
     }
 }
