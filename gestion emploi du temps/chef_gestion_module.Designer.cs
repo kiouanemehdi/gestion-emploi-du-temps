f06e6e5b57fcfbd2ce94182c95a55e8d85ccaed7
El Mehdi Kiouane <30392394+kiouanemehdi@users.noreply.github.com>
2019-11-30 20:52:53 +0100

    ......

--- gestion emploi du temps/chef_gestion_module.Designer.cs
+++ gestion emploi du temps/chef_gestion_module.Designer.cs
@@ -123,7 +123,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(150, 21);
             this.comboBox1.TabIndex = 18;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+           this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label2
             // 
